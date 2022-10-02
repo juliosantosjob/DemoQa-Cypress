0cb7a033756b53c39f1fff5698add2b8cacbaa58
julio958214 <julio958214@users.noreply.github.com>
2022-10-02 20:22:31 +0000

    deploy: c940700e72343311a58a6efdba2bf95af76c49e6

--- cypress/constants/elements.js
+++
@@ -1,8 +0,0 @@
-import { faker } from '@faker-js/faker';
-
-module.exports = {
-    name: faker.internet.userName(),
-    email: faker.internet.email(),
-    currentAddress: faker.address.streetAddress(),
-    permanentAddress: faker.address.streetAddress(),
-};

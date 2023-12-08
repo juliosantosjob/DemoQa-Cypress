16312c00e4cf0cc7b6eb7772b567f7077cbdeb70
Julio <julio958214@gmail.com>
2023-12-08 12:24:03 -0300

    code refactory

--- cypress/e2e/elements.cy.js
+++ cypress/e2e/elements.cy.js
@@ -1,10 +1,9 @@
 import data from '../support/radomData';
 
 describe('Interface elements', () => {
-    
+
     it('Validating textbox', () => {
         cy.visit('/elements');
-        cy.get('.main-header').should('contain', 'Elements');
         cy.contains('Text Box').click();
 
         cy.get('#userName').type(data.firstName);
@@ -12,20 +11,19 @@ describe('Interface elements', () => {
 
         cy.get('#currentAddress').type(data.currentAddress);
         cy.get('#permanentAddress').type(data.permanentAddress);
-        cy.get('[type="button"]').last().click();
+        cy.get('#submit').click();
 
         cy.get('#name').should('contain', data.firstName);
         cy.get('#email').should('contain', data.email);
 
-        cy.get('.border').find('#currentAddress').should('contain', data.currentAddress);
-        cy.get('.border').find('#permanentAddress').should('contain', data.permanentAddress);
+        cy.get('.border #currentAddress').should('contain', data.currentAddress);
+        cy.get('.border #permanentAddress').should('contain', data.permanentAddress);
     });
 
     it('Validating checKbox', () => {
         cy.visit('/checkbox');
-        cy.get('.main-header').should('contain', 'Check Box');
-
         cy.get('.rct-collapse .rct-icon').click();
+
         cy.get('svg[class*="rct-icon rct-icon-expand-c"]').eq(1).click();
         cy.get('button[class="rct-collapse rct-collapse-btn"]').eq(3).click();
 
@@ -33,12 +31,12 @@ describe('Interface elements', () => {
         cy.get('#tree-node-react').should('be.checked');
     });
 
-    it('Validating radio button', () => {
+    it.only('Validating radio button', () => {
         cy.visit('/radio-button');
-        cy.get('.main-header').should('contain', 'Radio Button');
-        cy.get('#yesRadio').click({ force: true }).check();
 
+        cy.get('#yesRadio').check({ force: true });
         cy.get('#yesRadio').should('be.checked');
+
         cy.get('#impressiveRadio').should('not.be.checked');
         cy.get('#noRadio').should('not.be.checked');
     });
